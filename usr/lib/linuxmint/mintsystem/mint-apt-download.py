376e011ce3559f8c940a3e6142dbae2efb9b9a04
gm10 <13855078+gm10@users.noreply.github.com>
2018-12-14 00:06:36 +0100

    Complete overhaul of apt and related scripts:
    Modified and Fixed Commands:
    - add-repository works again
    - changelog command can now retrieve full changelogs for almost any
      package and retrieves changelogs for installed packages locally
      (unless --no-local was specified) - if download size exceeds 2.5 MB
      the user is asked for confirmation
    - download rewritten:
      - now supports version and release declaration,
      - now supports multiple packages in the argument,
      - only creates a .tar.gz if at 2 packages were downloaded,
      - no longer risks overwriting files in the home folder,
      - only refreshes the cache if older than 24h, and
      - has improved error checking, handling and messaging, including
        handling of broken dependencies
    - edit-sources is now using nano directly instead of binary apt since
      the latter cannot open symlinked source files
    - recommends rewritten, now defaults to non-recursive mode and has a
      -R / --recursive parameter for recursive mode
    - search gained an optional --apt parameter to use apt instead of
      aptitude for the search
    - version is now running dpkg-query directly instead of going through
      an external script
    - help output reformatted for easier readability
    - binary apt is now used for most commands, where available
    
    New Commands:
    - auto-clean & auto-remove aliases added for binary apt compatibility
    - add-key: download and add key id from keyserver or from file
    - listi: short for list --installed
    - listu: short for list --upgradable
    - purgeall: short for purge --autoremove
    - purge-residual: purge residual configuration
    - showsrc: show source records
    - --version: binary apt version passthrough
    - unmarkauto: alias  for markmanual
    
    Removed Commands:
    - stats
    - sources (just the alias, too easy to confuse with the source command)
    
    Other Fixes and Changes:
    - options beginning with a dash can now be entered before the command
    - proper handling of quotes and escaped characters in parameters
    - bash completion rewritten, now supports all commands and most options
    - apt no longer asks for sudo privileges if a valid simulation argument
      was used
    - highlighting limited to aptitude search, now supports multiple search
      terms and handles parameters
    - Sorting and highlighting is now done internally
    - removed the no longer needed highlight-mint script
    - signals like Ctrl+C are handled, with signals that pause execution
      leading to termination to prevent lingering dpkg lock
    - usage text corrected and amended
    - added python3-apt dependency
    - added ./test script to the source as with other Mint packages
    - likely more

--- usr/lib/linuxmint/mintsystem/mint-apt-download.py
+++ usr/lib/linuxmint/mintsystem/mint-apt-download.py
@@ -1,42 +1,193 @@
 #!/usr/bin/python3
+import fnmatch
+import os
+import shutil
+import subprocess
+import sys
+import time
+from glob import glob
+from tempfile import gettempdir
 
 import apt
-import sys
-import subprocess
-import os
+
+
+def strtobool(val):
+    val = val.lower()
+    if val in ('y', 'yes'):
+        return True
+    elif val in ('n', 'no'):
+        return False
+    else:
+        raise ValueError("Invalid response value %r" % (val,))
+
+def get_user_input():
+    while True:
+        try:
+            retval = strtobool(input())
+            print("")
+            return retval
+        except ValueError:
+            print('Invalid response. Try again [y/n]: ', end='')
+        except KeyboardInterrupt:
+            pass
+
+def print_e(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def download_package(package, show_success_message=True):
+    try:
+        package.candidate.fetch_binary()
+        if show_success_message:
+            print('\nThe package "%s" was downloaded to the current directory as\n  %s' %
+                  (package.name, os.path.basename(package.candidate.filename)))
+    except Exception as e:
+        print_e(f"An error occured while downloading the package:\n{e}")
+        return False
+    return True
+
+def get_package(pkg_name):
+    # parse =version declaration
+    if "=" in pkg_name:
+        (pkg_name, pkg_version) = pkg_name.split("=", 1)
+        pkg_release = None
+    # parse /release declaration (only if no version specified)
+    elif "/" in pkg_name:
+        (pkg_name, pkg_release) = pkg_name.split("/", 1)
+        pkg_version = None
+    else:
+        pkg_version = None
+        pkg_release = None
+
+    # check if pkg_name exists
+    if pkg_name in cache.keys():
+        pkg = cache[pkg_name]
+    else:
+        print_e(f"E: Unable to locate package {pkg_name}")
+        return False
+
+    # get package data
+    _candidate = None
+    if pkg_release or pkg_version:
+        match_found = False
+        for _pkg in pkg.versions:
+            if pkg_version:
+                if fnmatch.fnmatch(_pkg.version, pkg_version):
+                    match_found = True
+            else:
+                for _origin in _pkg.origins:
+                    if fnmatch.fnmatch(_origin.archive, pkg_release):
+                        match_found = True
+            if match_found:
+                _candidate = _pkg
+                break
+        if not match_found:
+            if pkg_release:
+                print_e(f'E: Release "{pkg_release}" is unavailable for "{pkg.name}"')
+            else:
+                print_e(f'E: Version "{pkg_version}" is unavailable for "{pkg.name}"')
+            return False
+    else:
+        _candidate = pkg.candidate
+    pkg.candidate = _candidate
+    return pkg
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        package_name = sys.argv[1]
-        cache = apt.Cache()
+    if len(sys.argv) > 1:
+        # Update the cache first if older than 24h
+        try:
+            apt.apt_pkg.init_config()
+            pkgcache = apt.apt_pkg.config.find_file("Dir::Cache::pkgcache")
 
-        # Update the cache first
-        subprocess.call(["/usr/local/bin/apt", "update"])
-        print ("")
+            if not os.path.isfile(pkgcache) or \
+                time.time() - os.path.getmtime(pkgcache) > 24 * 60 * 60:
+                subprocess.call(["/usr/local/bin/apt", "update"])
+                print ("")
+        except:
+            pass
 
-        if package_name not in cache:
-            print("Error: package %s not found in APT cache!" % package_name)
+        # Check permissions to download to this folder
+        if not os.access(os.getcwd(), os.W_OK | os.X_OK):
+            print_e("E: Current folder is not writable")
             sys.exit(1)
 
-        package = cache[package_name]
-        if package.is_installed:
-            os.system("/usr/bin/apt download %s" % package.name)
-            print("")
-            print("The package %s is already installed. Its deb file was downloaded in the current directory." % package.name)
-        else:
-            subprocess.call(["mkdir", "-p", package.name])
-            os.chdir(package.name)
-            os.system("rm -rf *.deb")
-            os.system("LC_ALL=C apt-get install --dry-run %s | grep ^Inst | awk {'print $2;'} | xargs apt-get download " % package.name)
-            os.system("echo 'sudo dpkg -i *.deb' > install.sh")
-            os.system("chmod a+rx install.sh")
-            os.chdir("..")
-            os.system("tar cvf %s.tar %s/ >/dev/null" % (package.name, package.name))
-            os.system("gzip %s.tar" % package.name)
-            os.system("rm -rf '%s'" % package.name)
-            print("")
-            print("The packages were downloaded into %s.tgz. This archive contains all the packages necessary for an offline installation." % package.name)
+        # Load cache
+        cache = apt.Cache()
 
+        # Get cwd
+        pwd = os.getcwd()
+
+        for package_name_full in sys.argv[1:]:
+            # Skip option parameter, we support none
+            if package_name_full.startswith("-"):
+                continue
+
+            # Get package
+            package = get_package(package_name_full)
+            if not package:
+                continue
+
+            # Check if package is downloadable
+            if not package.candidate.downloadable:
+                print_e(f'E: No download source available for "{package_name_full}"')
+                continue
+
+            # INSTALLED package
+            if package.candidate.is_installed:
+                # Download installed package as .deb
+                download_package(package)
+                continue
+
+            # NOT INSTALLED package
+            package.mark_install()
+            package_list = cache.get_changes()
+            if not package_list or len(package_list) == 1:
+                # Broken dependencies or only single package:
+                # download stand-alone package as .deb instead
+                download_package(package)
+                continue
+
+            # Check if target file already exists
+            archive_name = f"{package.name}_{package.candidate.version}.tar.gz"
+            if os.path.exists(archive_name):
+                print(f'W: Target archive "{archive_name}" already exists. Overwrite? [y/n]: ', end='')
+                response = get_user_input()
+                if response == True:
+                    os.unlink(archive_name)
+                else:
+                    continue
+
+            # Download package incl. missing dependencies and compress as .tar.gz
+            tmpfolder = None
+            while not tmpfolder:
+                _tmpfolder = os.path.join(gettempdir(), f"mint-apt-download-{package.name}-{hash(os.times())}")
+                if not os.path.exists(_tmpfolder):
+                    tmpfolder = _tmpfolder
+            os.makedirs(tmpfolder)
+            os.chdir(tmpfolder)
+            try:
+                for _package in package_list:
+                    if _package.marked_delete:
+                        continue
+                    success = download_package(_package, False)
+                    if not success:
+                        raise ValueError(f"An error occured downloading the package: {_package.name}")
+                with open(os.open("install.sh", os.O_CREAT | os.O_WRONLY, 0o775), 'w') as f:
+                    f.write("sudo dpkg -i *.deb\n")
+                subprocess.run(["tar", "-zcvf", f"{pwd}/{archive_name}"] + glob("*"),
+                    stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, check=True)
+                print(f"""
+All the packages necessary for offline installation of
+  {package_name_full}
+were downloaded to the current directory into the archive:
+  {archive_name}
+Extract into a folder and run the extracted install.sh to install.""")
+            except subprocess.CalledProcessError as e:
+                print_e(f"E: An error occured creating the archive:\n{e.stderr.decode()}")
+            except ValueError as e:
+                print_e(f"E: {e}")
+            # Remove temporary files and folder
+            shutil.rmtree(tmpfolder)
+        os.chdir(pwd)
     else:
         print("Usage: apt download [package]")
         sys.exit(1)
--- usr/lib/linuxmint/mintsystem/mint-apt-recommends.py
+++ usr/lib/linuxmint/mintsystem/mint-apt-recommends.py
@@ -1,84 +1,89 @@
 #!/usr/bin/python3
 
-import apt
 import sys
-import subprocess
+
+import apt
 
 
 class RecommendsFinder:
 
-    def __init__(self, cache, package):
-        self.cache = cache
-        self.package = package
-        self.missing_recommends = []
-        output = subprocess.getoutput("aptitude search '?broken-reverse-recommends(?installed)' | awk {'print $2;'}")
-        for line in output.split("\n"):
-            line = line.strip()
-            self.missing_recommends.append(line)
-
+    def __init__(self, _cache, _package, recursive):
+        self.cache = _cache
         self.found_missing_recommends = []
         self.already_looked_at = []
 
-        self.get_recommends(self.package, 1)
+        if recursive:
+            level = 1
+        else:
+            level = 0
+        self.get_recommends(_package, level)
 
-    def get_recommends(self, package, level):
-        if package.name not in self.already_looked_at:
-
-            # Uncomment this to debug ;)
-            # prefix = ""
-            # for i in range(level):
-            #     prefix = " %s" % prefix
-            # print prefix, package.name
-
-            self.already_looked_at.append(package.name)
-            if package.is_installed:
-                pkg = package.installed
+    def get_recommends(self, pkg, level):
+        if pkg.name not in self.already_looked_at:
+            self.already_looked_at.append(pkg.name)
+            if pkg.is_installed:
+                pkg = pkg.installed
             else:
-                pkg = package.candidate
+                pkg = pkg.candidate
             # go through the recommends
             for recommend in pkg.recommends:
                 for base_rec in recommend.or_dependencies:
-                    if base_rec.name in self.missing_recommends and base_rec.name not in self.found_missing_recommends:
-                        self.found_missing_recommends.append(base_rec.name)
-                        if base_rec.name in self.cache:
-                            rec_pkg = self.cache[base_rec.name]
-                            self.get_recommends(rec_pkg, level + 1)
+                    rec_name = base_rec.name
+                    if (rec_name not in self.found_missing_recommends and
+                        rec_name in self.cache and
+                        not self.cache[rec_name].is_installed
+                        ):
+                        self.found_missing_recommends.append(rec_name)
+                        if level > 0:
+                            self.get_recommends(self.cache[rec_name], level + 1)
             # go through the dependencies
             for dep in pkg.dependencies:
                 for base_dep in dep.or_dependencies:
                     dep_name = base_dep.name
                     if dep_name in self.cache:
                         dep_pkg = self.cache[dep_name]
-                        # if the package is installed but the dep isn't, ignore it (it means another dep honored the dependency)
-                        if package.is_installed:
-                            if not dep_pkg.is_installed:
-                                continue
-                        self.get_recommends(dep_pkg, level + 1)
+                        # if the package is installed but the dep isn't, ignore it
+                        # (it means another dep honored the dependency)
+                        if pkg.is_installed and not dep_pkg.is_installed:
+                            continue
+                        if level > 0:
+                            self.get_recommends(dep_pkg, level + 1)
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        package_name = sys.argv[1]
+    package_name = next((item for item in sys.argv[1:] if not item.startswith("-")), None)
+    if len(sys.argv) in (2, 3) and package_name:
         cache = apt.Cache()
-        if package_name in cache:
+        if package_name in cache.keys():
             package = cache[package_name]
-            finder = RecommendsFinder(cache, package)
+            finder = RecommendsFinder(cache, package,
+                [x for x in ["--recursive","-R"] if x in sys.argv[1:]])
             missing_recommends = sorted(finder.found_missing_recommends)
-            print("")
             if len(missing_recommends) > 0:
-                print("The following missing recommended packages were found for %s:\n" % package_name)
+                if len(missing_recommends) > 1:
+                    msg = "The following missing recommended packages were found for"
+                else:
+                    msg = "The following missing recommended package was found for"
+                print(msg + " %s:\n" % package_name)
                 for missing in missing_recommends:
                     print("    %s" % missing)
-                print("")
-                print("You can install them by typing the following command:")
-                print("")
-                print("    apt install %s" % " ".join(missing_recommends))
+                if len(missing_recommends) > 1:
+                    msg = "You can install them by running the following command:"
+                else:
+                    msg = "You can install it by running the following command:"
+                print("\n%s\n\n    apt install %s\n" % (msg, " ".join(missing_recommends)))
             else:
                 print("No missing recommended packages were found for %s" % package_name)
-            print("")
         else:
-            print("Error: package %s not found in APT cache!" % package_name)
+            print("E: Unable to locate package %s" % package_name, file=sys.stderr)
             sys.exit(1)
-
     else:
-        print("Usage:  apt recommends [package]")
+        print("""\
+Usage:  apt recommends [options] <package>
+
+List not installed recommended packages for the given package.
+
+Options:
+        -R | --recursive
+            Also list missing recommended packages of dependent or
+            recommended packages.""")
         sys.exit(1)
